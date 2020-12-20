a5e26bee5abff794475a762fa93e72fca7f7f687
Gustavo Rupp <gustavorupp@gmail.com>
2020-12-20 14:11:24 -0300

    static and dynamic

---
+++ pages/tempo.js
@@ -0,0 +1,24 @@
+function Tempo(props) {
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return (
+        <div>
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (static)</div>
+        </div>
+    )
+}
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
+}
+
+export default Tempo;
